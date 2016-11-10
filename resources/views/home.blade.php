710f51da3292076a8ab6cdce0e7edda9e67482e9
Mael Mayon <mael.mayon@free.fr>
2016-11-10 16:32:40 +0100

    Fail Commit Master - Centrer les Blocks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,12 +18,51 @@
     </div>
 </section>
 <section class="container-fluid">
-    <h2>Mon Blog</h2>
+    <div class="container">
+        <div class="row">
+            <h2>Compétences</h2>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 blockHome">
+                <p>
+                    Développeur en herbe découvrant encore les joie du Web
+                </p>
+                <figure>
+                    <img src="http://placehold.it/500x500" alt="Photo de moi">
+                    <figcaption>Votre programmeur fétishe</figcaption>
+                </figure>
+            </div>
+        </div>
+    </div>
 </section>
 <section class="container-fluid">
-    <h2>Outils Utiles</h2>
+    <div class="container">
+        <div class="row">
+            <h2>Blog</h2>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 blockHome">
+                <p>
+                    Développeur en herbe découvrant encore les joie du Web
+                </p>
+                <figure>
+                    <img src="http://placehold.it/500x500" alt="Photo de moi">
+                    <figcaption>Votre programmeur fétishe</figcaption>
+                </figure>
+            </div>
+        </div>
+    </div>
 </section>
 <section class="container-fluid">
-    <h2>Contact</h2>
+    <div class="container">
+        <div class="row">
+            <h2>Compétences</h2>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 blockHome">
+                <p>
+                    Développeur en herbe découvrant encore les joie du Web
+                </p>
+                <figure>
+                    <img src="http://placehold.it/500x500" alt="Photo de moi">
+                    <figcaption>Votre programmeur fétishe</figcaption>
+                </figure>
+            </div>
+        </div>
+    </div>
 </section>
 @endsection('content')
