85364c6285bd8f41403ae0d9f5781c8c4195d6dd
Mael Mayon <mael.mayon@free.fr>
2016-11-10 12:04:50 +0100

    Modif Blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,22 @@
 
 @section('content')
 <section class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12"></div>
+            <div class="col-sm-8 col-sm-offset-2"></div>
+        </div>
+    </div>
     <h2>Qui suis-je ?</h2>
+    <div class="">
+        <p>
+            Développeur en herbe découvrant encore les joie du Web
+        </p>
+        <figure>
+            <img src="http://placehold.it/500x500" alt="Photo de moi">
+            <figcaption>Votre programmeur fétishe</figcaption>
+        </figure>
+    </div>
 </section>
 <section class="container-fluid">
     <h2>Mon Blog</h2>
