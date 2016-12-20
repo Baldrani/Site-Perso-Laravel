bdc929f285d2c7ff2d5263eb6d10580e6c3d1dab
Mael Mayon <mael.mayon@free.fr>
2016-12-20 17:58:04 +0100

    Semi Fix

--- resources/views/blog/add.blade.php
+++ resources/views/blog/add.blade.php
@@ -31,50 +31,14 @@
                 <label for="title">Date :</label><br>
                 <input type="text" name="date" id="date" style="width:100%;" value="{{$article->date}}"><br><br>
                 <label for="content">Content :</label><br>
-                <div id="editor">
-				</div>
-                <script>
-                    if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 )
-                    	CKEDITOR.tools.enableHtml5Elements( document );
-
-                    // The trick to keep the editor in the sample quite small
-                    // unless user specified own height.
-                    CKEDITOR.config.height = 150;
-                    CKEDITOR.config.width = 'auto';
-
-                    var initSample = ( function() {
-                    	var wysiwygareaAvailable = isWysiwygareaAvailable(),
-                    		isBBCodeBuiltIn = !!CKEDITOR.plugins.get( 'bbcode' );
-
-                    	return function() {
-                    		var editorElement = CKEDITOR.document.getById( 'editor' );
-
-                    		// Depending on the wysiwygare plugin availability initialize classic or inline editor.
-                    		if ( wysiwygareaAvailable ) {
-                    			CKEDITOR.replace( 'editor' );
-                    		} else {
-                    			editorElement.setAttribute( 'contenteditable', 'true' );
-                    			CKEDITOR.inline( 'editor' );
-
-                    			// TODO we can consider displaying some info box that
-                    			// without wysiwygarea the classic editor may not work.
-                    		}
-                    	};
-
-                    	function isWysiwygareaAvailable() {
-                    		// If in development mode, then the wysiwygarea must be available.
-                    		// Split REV into two strings so builder does not replace it :D.
-                    		if ( CKEDITOR.revision == ( '%RE' + 'V%' ) ) {
-                    			return true;
-                    		}
-
-                    		return !!CKEDITOR.plugins.get( 'wysiwygarea' );
-                    	}
-                    } )();
-                    initSample();
-                </script>
+                <textarea name="content" id="content1" rows="10" cols="80">
+                    {{$article->content}}
+                </textarea>
                 <input type="hidden" value="{{$article->id}}" name="id">
-
+                <script>
+                CKEDITOR.replace( 'content1' );
+                // $('[href="/css/app.css"]').remove()
+                </script>
             </form>
             <br>
             <button type="button" class="publish" style="margin:auto;display:block;background-color: rgb(0, 140, 14);color: #fff;">Publier</button> <br>
